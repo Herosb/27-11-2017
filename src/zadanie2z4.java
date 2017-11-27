48272c25f38742b2e09fe7970be3b65f8f0a3708
Student <Student@10.2.5.4>
2017-11-27 17:50:43 +0100

    nie wypisuje od tyłu

---
+++ src/zadanie2z4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class zadanie2z4 {
+
+	public static void main(String[] args) {
+		int n;
+		System.out.println("Podaj ciag znakow");
+		String znaki;
+		Scanner odczyt = new Scanner(System.in);
+		znaki = odczyt.nextLine();
+		for (int i = 0; i < znaki.length(); i++) {
+			
+			
+			
+			System.out.print(+i);
+		}
+	}
+}
